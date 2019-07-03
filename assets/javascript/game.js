44d3e369ba04ec95cb295c00803e31ce0f1cb076
Edgar Ayala <edgarayala@Edgars-MacBook-Pro.local>
2019-07-03 02:00:29 -0400

    wrote function for images to transfer intoa  new div when selected

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+
+    
+$(".agumon").on("click", function(){
+$(".agumon").appendTo("#agumonDestination");
+})
+
+$(".gabumon").on("click", function(){
+$(".gabumon").appendTo("#gabumonDestination");
+})
+
+$(".gomamon").on("click", function(){
+$(".gomamon").appendTo("#gomamonDestination");
+})
+
+$(".patamon").on("click", function(){
+$(".patamon").appendTo("#patamonDestination").css({float:right});
+
+})
+
+
+
+
+
+
+
+
+})
