b37b211d1b6f2a3b1f749b28697f7cac6683a59c
Edgar Ayala <edgarayala@Edgars-MacBook-Pro.local>
2019-07-03 22:19:57 -0400

    fixed issue where characters wouldnt display inline

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,37 @@
 $(document).ready(function() {
 
 
-    
+    //move characters from starting position to enemy position after selecting character
 $(".agumon").on("click", function(){
-$(".agumon").appendTo("#agumonDestination");
+$(".gabumon").appendTo("#gabumonDestination").css('display','inline-block');    
+$(".gomamon").appendTo("#gomamonDestination").css('display','inline-block');
+$(".patamon").appendTo("#patamonDestination").css('display','inline-block');
 })
 
 $(".gabumon").on("click", function(){
-$(".gabumon").appendTo("#gabumonDestination");
+$(".agumon").appendTo("#agumonDestination").css('display', 'inline');
+$(".gomamon").appendTo("#gomamonDestination").css('display', 'inline');
+$(".patamon").appendTo("#patamonDestination").css('display', 'inline');
 })
 
 $(".gomamon").on("click", function(){
-$(".gomamon").appendTo("#gomamonDestination");
+$(".agumon").appendTo("#agumonDestination");
+$(".gabumon").appendTo("#gabumonDestination");
+$(".patamon").appendTo("#patamonDestination");
 })
 
 $(".patamon").on("click", function(){
-$(".patamon").appendTo("#patamonDestination").css({float:right});
-
+$(".agumon").appendTo("#agumonDestination");
+$(".gabumon").appendTo("#gabumonDestination");    
+$(".gomamon").appendTo("#gomamonDestination");
 })
 
+//move character from enemies available to attack to the defender section
 
+$("#agumonDestination").on("click", function(){
+    
 
+})
 
 
 
