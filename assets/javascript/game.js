0ed8bb4d92b9a6181ace2e4d64a2a2c54e27e7f4
Edgar Ayala <edgarayala@Edgars-MacBook-Pro.local>
2019-07-10 21:43:56 -0400

    made an attack function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,90 @@
 $(document).ready(function() {
 
 
-    //move characters from starting position to enemy position after selecting character
-$(".agumon").on("click", function(){
-$(".gabumon").appendTo("#gabumonDestination").css('display','inline');    
-$(".gomamon").appendTo("#gomamonDestination").css('display','inline');
-$(".patamon").appendTo("#patamonDestination").css('display','inline');
-})
+var agumon = 
+{   
+    Name: "agumon",
+    image: "agumon",
+    lifepoints: 150,
+    damage: 15,
+    damageMultiplier:2,
 
-$(".gabumon").on("click", function(){
-$(".agumon").appendTo("#agumonDestination").css('display', 'inline');
-$(".gomamon").appendTo("#gomamonDestination").css('display', 'inline');
-$(".patamon").appendTo("#patamonDestination").css('display', 'inline');
-})
+}
+
+var gabumon = 
+{   
+    Name: "gabumon",
+    lifepoints: 150,
+    damage:24,
+    damageMultiplier:4,
+}
+
+var gomamon = 
+{   
+    Name: "gomamon",
+    lifepoints: 150,
+    damage: 15,
+    damageMultiplier:3,
+}
+
+var patamon = 
+{   
+    Name: "patamon",
+    lifepoints: 150,
+    damage:20,
+    damageMultiplier:5,
+}
+
+
+function chooseCharacter()
+{
+    
+}
+
+
+
+
+
+    //move characters from starting position to enemy position after selecting character
+    $(".agumon").on("click", function(){
+    $(".agumon").html("")
+    })
+
+
+
+
 
 $(".gomamon").on("click", function(){
 $(".agumon").appendTo("#agumonDestination").css('display', 'inline');
 $(".gabumon").appendTo("#gabumonDestination").css('display', 'inline');
 $(".patamon").appendTo("#patamonDestination").css('display', 'inline');
+return
 })
 
 $(".patamon").on("click", function(){
 $(".agumon").appendTo("#agumonDestination").css('display', 'inline');
 $(".gabumon").appendTo("#gabumonDestination").css('display', 'inline');    
 $(".gomamon").appendTo("#gomamonDestination").css('display', 'inline');
+return
 })
 
 //move character from enemies available to attack to the defender section
 
 $("#agumonDestination").on("click", function(){
     $(".agumon").appendTo("#agumonDestinationDefender")
+    return
 })
 $("#gabumonDestination").on("click", function(){
     $(".gabumon").appendTo("#gabumonDestinationDefender")
+    return
 })
 $("#gomamonDestination").on("click", function(){
     $(".gomamon").appendTo("#gomamonDestinationDefender")
+    return
 })
 $("#patamonDestination").on("click", function(){
     $(".patamon").appendTo("#patamonDestinationDefender")
+    return
 })
 
 
@@ -50,14 +96,50 @@ var gabumonlp = 150
 var gomamonlp = 110
 var patamonlp = 200
 
-function lifepoints()
+
+
+function agumonlifepointsfunc()
 {
-$("patamonlifepoints").html("<h1>patamonlp</h1>")
+$("#agumonlifepoints").html(agumonlp)
+}
+agumonlifepointsfunc()
+
+
+function gabumonlifepointsfunc()
+{
+$(".gabumonlifepoints").html(gabumonlp)
+}
+gabumonlifepointsfunc()
+
+
+function gomamonlifepointsfunc()
+{
+$(".gomamonlifepoints").html(gomamonlp)
+}
+gomamonlifepointsfunc()
+
+
+function patamonlifepointsfunc()
+{
+$(".patamonlifepoints").html(patamonlp)
+}
+patamonlifepointsfunc()
+
+
+
+function  attack(){
+    
+    
+
 }
 
 
 
-lifepoints()
+
+    document.getElementById('attackbutton').onclick = function(){
+   
+        document.getElementById('agumonlifepoints').innerHTML= agumonlp--;
+    }
 
 
 
